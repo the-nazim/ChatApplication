965d33dce03973301e7a22f55967fcae6b8be73d
the-nazim <thenazim0707@gmail.com>
2025-04-26 11:27:50 +0000

    Initial commit

---
+++ ChatApp/server/server.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+using namespace std;
+int main()
+{
+    int serverSocket, clientSocket;
+    
+}
+
+
+
