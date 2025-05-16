b2e1ff77bb3e10299757141f80fe8905eb5d8a53
the-nazim <thenazim0707@gmail.com>
2025-05-16 18:40:13 +0000

    Client database integrated

--- ChatApp/Client1/src/client.cpp
+++ ChatApp/Client1/src/client.cpp
@@ -1,49 +1,49 @@
 #include "../include/client.h"
 
-// string dbClient::registerClient()
-// {
-//     string username, password;
-//     cout<<"Enter username: ";
-//     cin>>username;
-//     cout<<"Enter password: ";
-//     cin>>password;
+string dbClient::registerClient()
+{
+    string username, password;
+    cout<<"Enter username: ";
+    cin>>username;
+    cout<<"Enter password: ";
+    cin>>password;
 
-//     bsoncxx::builder::basic::document doc_builder{};
-//     doc_builder.append(bsoncxx::builder::basic::kvp("username", username));
-//     doc_builder.append(bsoncxx::builder::basic::kvp("password", password));
-//     doc_builder.append(bsoncxx::builder::basic::kvp("timestamp", bsoncxx::types::b_date{std::chrono::system_clock::now()}));
+    bsoncxx::builder::basic::document doc_builder{};
+    doc_builder.append(bsoncxx::builder::basic::kvp("username", username));
+    doc_builder.append(bsoncxx::builder::basic::kvp("password", password));
+    doc_builder.append(bsoncxx::builder::basic::kvp("timestamp", bsoncxx::types::b_date{std::chrono::system_clock::now()}));
 
-//     auto result = collection.insert_one(doc_builder.view());
-//     cout<< "Inserted document ID: " << result->inserted_id().get_oid().value.to_string() << endl;
-//     cout << "Registered client: " << username << endl;
+    auto result = collection.insert_one(doc_builder.view());
+    cout<< "Inserted document ID: " << result->inserted_id().get_oid().value.to_string() << endl;
+    cout << "Registered client: " << username << endl;
 
-//     return username;
-// }
+    return username;
+}
 
-// string dbClient::loginClient()
-// {
-//     string username, password;
-//     cout<<"Enter username: ";
-//     cin>>username;
-//     cout<<"Enter password: ";
-//     cin>>password;
+string dbClient::loginClient()
+{
+    string username, password;
+    cout<<"Enter username: ";
+    cin>>username;
+    cout<<"Enter password: ";
+    cin>>password;
 
-//     auto cursor = collection.find(bsoncxx::builder::basic::make_document(
-//         bsoncxx::builder::basic::kvp("username", username),
-//         bsoncxx::builder::basic::kvp("password", password)
-//     ));
+    auto cursor = collection.find(bsoncxx::builder::basic::make_document(
+        bsoncxx::builder::basic::kvp("username", username),
+        bsoncxx::builder::basic::kvp("password", password)
+    ));
 
-//     if (cursor.begin() != cursor.end())
-//     {
-//         cout << "Login successful for user: " << username << endl;
-//     }
-//     else
-//     {
-//         cout << "Login failed for user: " << username << endl;
-//     }
+    if (cursor.begin() != cursor.end())
+    {
+        cout << "Login successful for user: " << username << endl;
+    }
+    else
+    {
+        cout << "Login failed for user: " << username << endl;
+    }
 
-//     return username;
-// }
+    return username;
+}
 
 void broadcastMessage(int socket)
 {
--- ChatApp/Client2/include/client.h
+++ ChatApp/Client2/include/client.h
@@ -1,7 +1,7 @@
-// #include <mongocxx/client.hpp>
-// #include <mongocxx/instance.hpp>
-// #include <mongocxx/uri.hpp>
-// #include <bsoncxx/json.hpp>
+#include <mongocxx/client.hpp>
+#include <mongocxx/instance.hpp>
+#include <mongocxx/uri.hpp>
+#include <bsoncxx/json.hpp>
 #include <bits/stdc++.h>
 #include <iostream>
 #include <netinet/in.h>
@@ -18,27 +18,27 @@ using namespace std;
 
 void broadcastMessage(int socket);
 
-// class dbClient
-// {
-//     private:
-//         mongocxx::instance inst;
-//         mongocxx::database db;
-//         mongocxx::client conn;
-//         mongocxx::collection collection;
+class dbClient
+{
+    private:
+        mongocxx::instance inst;
+        mongocxx::database db;
+        mongocxx::client conn;
+        mongocxx::collection collection;
 
-//         string db_name = "testdb";
-//         string collection_name = "test_clients";
+        string db_name = "testdb";
+        string collection_name = "test_clients";
 
-//     public:
-//         dbClient(): inst(), conn{mongocxx::uri{"mongodb://172.19.64.1:27017/"}}, db_name("testdb"), collection_name("test_clients")
-//         {
-//             db = conn[db_name];
-//             collection = db[collection_name];
-//         }
+    public:
+        dbClient(): inst(), conn{mongocxx::uri{"mongodb://172.19.64.1:27017/"}}, db_name("testdb"), collection_name("test_clients")
+        {
+            db = conn[db_name];
+            collection = db[collection_name];
+        }
 
-//         string registerClient();
-//         string loginClient();
-// };
+        string registerClient();
+        string loginClient();
+};
 
 class Client
 {
