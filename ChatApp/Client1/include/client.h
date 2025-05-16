c6cb0572739fed156ee56a19a96595b815e79389
the-nazim <thenazim0707@gmail.com>
2025-05-16 18:13:37 +0000

    Client-side code converted to oops

---
+++ ChatApp/Client1/include/client.h
@@ -0,0 +1,79 @@
+// #include <mongocxx/client.hpp>
+// #include <mongocxx/instance.hpp>
+// #include <mongocxx/uri.hpp>
+// #include <bsoncxx/json.hpp>
+#include <bits/stdc++.h>
+#include <iostream>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <thread>
+#include <cstring>
+#include <cstdlib>
+
+using namespace std;
+
+void broadcastMessage(int socket);
+
+// class dbClient
+// {
+//     private:
+//         mongocxx::instance inst;
+//         mongocxx::database db;
+//         mongocxx::client conn;
+//         mongocxx::collection collection;
+
+//         string db_name = "testdb";
+//         string collection_name = "test_clients";
+
+//     public:
+//         dbClient(): inst(), conn{mongocxx::uri{"mongodb://172.19.64.1:27017/"}}, db_name("testdb"), collection_name("test_clients")
+//         {
+//             db = conn[db_name];
+//             collection = db[collection_name];
+//         }
+
+//         string registerClient();
+//         string loginClient();
+// };
+
+class Client
+{
+    private:
+        int clientSocket;
+        sockaddr_in serverAddress;
+
+    public:
+        Client(string ipAddress, int port)
+        {
+            clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+            memset(&serverAddress, 0, sizeof(serverAddress));
+
+            serverAddress.sin_family = AF_INET;
+            serverAddress.sin_port = htons(port);
+            inet_pton(AF_INET, ipAddress.c_str(), &serverAddress.sin_addr);
+
+            if (clientSocket == -1)
+            {
+                cout << "Error in creating socket" << endl;
+                exit(1);
+            }
+
+            if (connect(clientSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) == -1)
+            {
+                cout << "Error in connecting to server" << endl;
+                exit(1);
+            }
+        }
+
+        void sendMessage();
+
+        ~Client()
+        {
+            close(clientSocket);
+        }
+};
---
+++ ChatApp/Client2/src/client.cpp
@@ -0,0 +1,98 @@
+#include "../include/client.h"
+
+// string dbClient::registerClient()
+// {
+//     string username, password;
+//     cout<<"Enter username: ";
+//     cin>>username;
+//     cout<<"Enter password: ";
+//     cin>>password;
+
+//     bsoncxx::builder::basic::document doc_builder{};
+//     doc_builder.append(bsoncxx::builder::basic::kvp("username", username));
+//     doc_builder.append(bsoncxx::builder::basic::kvp("password", password));
+//     doc_builder.append(bsoncxx::builder::basic::kvp("timestamp", bsoncxx::types::b_date{std::chrono::system_clock::now()}));
+
+//     auto result = collection.insert_one(doc_builder.view());
+//     cout<< "Inserted document ID: " << result->inserted_id().get_oid().value.to_string() << endl;
+//     cout << "Registered client: " << username << endl;
+
+//     return username;
+// }
+
+// string dbClient::loginClient()
+// {
+//     string username, password;
+//     cout<<"Enter username: ";
+//     cin>>username;
+//     cout<<"Enter password: ";
+//     cin>>password;
+
+//     auto cursor = collection.find(bsoncxx::builder::basic::make_document(
+//         bsoncxx::builder::basic::kvp("username", username),
+//         bsoncxx::builder::basic::kvp("password", password)
+//     ));
+
+//     if (cursor.begin() != cursor.end())
+//     {
+//         cout << "Login successful for user: " << username << endl;
+//     }
+//     else
+//     {
+//         cout << "Login failed for user: " << username << endl;
+//     }
+
+//     return username;
+// }
+
+void broadcastMessage(int socket)
+{
+    char buffer[1024];
+    while (true) 
+    {
+        memset(buffer, 0, sizeof(buffer));
+        int bytes = recv(socket, buffer, sizeof(buffer), 0);
+        if (bytes <= 0) {
+            cout << "Disconnected from server.\n";
+            break;
+        }
+        cout << "\nMessage from server: " << buffer << endl;
+    }
+}
+
+void Client::sendMessage()
+{
+    string message;
+
+    thread broadThread(broadcastMessage, clientSocket);
+
+    while (true) 
+    {
+        cout << "Enter the message: ";
+        getline(cin, message);
+
+        if (message == "exit") 
+            break;
+
+        send(clientSocket, message.c_str(), message.length(), 0);
+    }
+    broadThread.join(); // Wait for receive thread
+}
+
+
+int main(int argc, char *argv[])
+{
+    if (argc != 3) 
+    {
+        cout << "Usage: " << argv[0] << " <IP Address> <Port>" << endl;
+        return 1;
+    }
+
+    string ipAddress = argv[1];
+    int port = atoi(argv[2]);
+
+    Client client(ipAddress, port);
+    client.sendMessage();
+
+    return 0;
+}
